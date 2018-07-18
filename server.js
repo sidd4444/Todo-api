b6415852e60c3dbbf3b99231b5ca29a99f87e5ee
Sidd <Siddhant.jain@credencys.com>
2018-07-18 18:34:56 +0530

    Init repo

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+	res.send('todo api call');
+})
+
+app.listen(PORT, function() {
+	console.log('server started at:- ' + PORT);
+})
