ce54cc5b4f92ce47b4f035cc63b0e78c17f46851
Sidd <Siddhant.jain@credencys.com>
2018-07-20 12:19:17 +0530

    Add completed filter

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ var todoNextId = 1;
  
  app.use(bodyParser.json());
 
+// Get todos
 app.get('/todos', function(req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	if (queryParams.hasOwnProperty('completed') && (queryParams.completed === 'true')) {
+		filteredTodos = _.where(filteredTodos, { completed: true })
+	} else if (queryParams.hasOwnProperty('completed') && (queryParams.completed === 'false')) {
+		filteredTodos = _.where(filteredTodos, { completed: false })
+	}
+	res.json(filteredTodos);
 })
 
 app.get('/todos/:id', function(req, res) {
 	var matchedTodo = _.findWhere(todos, {id: parseInt(req.params.id)});
-	// todos.forEach(function(todo) {
-	// 	console.log(todo.id);
-	// 	if (parseInt(req.params.id) === todo.id) {
-	// 		matchedTodo = todo;
-			
-	// 	}
-	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
