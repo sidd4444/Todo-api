4b97238a43b3ad5f5fdb3eae05e4b6c47f2e4658
Sidd <Siddhant.jain@credencys.com>
2018-08-03 17:22:25 +0530

    Added auth token generation to post users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -55,13 +57,13 @@ module.exports = function(sequelize, DataTypes) {
 					}).then(function(userFound) {
 						console.log(!userFound);
 						if (userFound && bcrypt.compareSync(body.password, userFound.get('password_hash'))) {
-							return resolve(user);
+							 resolve(userFound);
 						} else {
 							return reject();
 						}
 
 					}, function(error) {
-						return reject();
+						 reject();
 					});
 				});
 			}
@@ -70,9 +72,26 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			 },
+			generateToken: function(type) {
+				console.log('inside' + type);
+				 if (!_.isString(type)) {
+				 	return undefined;
+				 }
+
+				 try {
+				 	var stringData = JSON.stringify({ id: this.get('id'), type: type});
+				 	var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+				 	var token = jwt.sign({
+				 		token: encryptedData
+				 	}, 'qwerty098');
+				 	return token;
+				 } catch(e) {
+				 	return undefined;
+				 }
 			}
 		}
 	});
-
-	return user;
+return user;
+	
 };
--- server.js
+++ server.js
@@ -210,20 +210,34 @@ app.post('/users', function(req, res) {
 /// Users Login
 //
 app.post('/users/login', function(req, res) {
-	
-	var body = _.pick(req.body, 'email', 'password');
-	db.user.authenticate(body).then(function(user) {
-		res.status(200).send(body);
-	}, function(error) {
-		console.log(error);
-		res.status(500).send();
-	});
-});
 
-// Sync
-db.sequelize.sync({ force: true}).then(function() {
-	// Listner
-	app.listen(PORT, function() {
-		console.log('server started at:- ' + PORT);
-	})
-});
+			var body = _.pick(req.body, 'email', 'password');
+			// db.user.authenticate(body).then(function(user) {
+			// 	res.json(user.toPublicJSON());
+			// }, function() {
+			// 	res.status(401).send()
+			// });
+			db.user.authenticate(body).then(function(user) {
+					var token = user.generateToken('authentication');
+					if (token) {
+						res.header('Auth', token).json(user.toPublicJSON());
+						}
+						else {
+							res.status(401).send();
+						}
+					},
+					function(error) {
+						console.log(error);
+						res.status(401).send();
+					});
+			});
+
+		// Sync
+		db.sequelize.sync({
+			force: true
+		}).then(function() {
+			// Listner
+			app.listen(PORT, function() {
+				console.log('server started at:- ' + PORT);
+			})
+		});
